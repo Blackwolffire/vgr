70750d1acf9ea24f02ed962c37b5e21442e3d5f6
Coralien Toso <coralien.toso@epita.fr>
2018-11-11 00:35:44 +0100

    Add load texture in header graphic

--- inc/graphical_engine.h
+++ inc/graphical_engine.h
@@ -3,6 +3,7 @@
 
 #include "engine.h"
 
+void load_textures(struct game_state *ga_st);
 void update_graphic(struct game_state *ga_st);
 void init_sdl(struct game_state *ga_st);
 void free_sdl(struct game_state *ga_st);
