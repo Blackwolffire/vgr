fa8d746578d83e647496acb8637d06891d0b96e5
Pia Consigny <pia.consigny@epita.fr>
2018-11-10 21:08:16 +0100

    Selector working, quit cmd ok

--- src/menu.c
+++ src/menu.c
@@ -1,22 +1,24 @@
 #include <err.h>
 #include "menu.h"
 
-void perform_selec(enum selec selec, struct game_state *ga_st)
+void perform_selec(enum selec selec, struct game_state *ga_st, int *state)
 {
     ga_st = ga_st;
     if (selec == QUIT)
+    {
+        *state = 0;
         SDL_Quit();
-
+    }
 
     else
-        SDL_Delay(500);
+        SDL_Delay(400);
 }
 
-void manage_update(enum selec selec, struct game_state *ga_st, int enter)
+void manage_update(enum selec selec, struct game_state *ga_st, int enter, int *state)
 {
     if (enter == 1)
     {
-        perform_selec(selec, ga_st);
+        perform_selec(selec, ga_st, state);
         return;
     }
     SDL_Rect pos_bg;
@@ -103,42 +105,42 @@ void menu(struct game_state *ga_st)
     int state = 1;
     int enter_selec = 0;
 
-    //while (state == 1)
-    //{
-    // SDL_WaitEvent(&event);
-    manage_update(4, ga_st, 0);
-    perform_selec(PLAY, ga_st);
-    //while (SDL_PollEvent(&event))
-    //{
-    /* if (event.type == SDL_QUIT)
-       {
-       state = 0;
-       break;
-       }
-       const Uint8 *key = SDL_GetKeyboardState(NULL);
+    while (state == 1)
+    {
+        SDL_WaitEvent(&event);
+        //manage_update(4, ga_st, 0);
+        //perform_selec(PLAY, ga_st);
+        //while (SDL_PollEvent(&event))
+        //{
+        if (event.type == SDL_QUIT)
+        {
+            state = 0;
+            break;
+        }
+        const Uint8 *key = SDL_GetKeyboardState(NULL);
 
-       if (key[SDL_SCANCODE_RETURN])
-       enter_selec = 1;
+        if (key[SDL_SCANCODE_RETURN])
+            enter_selec = 1;
 
-       else if (key[SDL_SCANCODE_UP])
-       {
-       if (selec == 0)
-       selec = COPYRIGHT;
-       else
-       selec -= 1;
-       }
+        else if (key[SDL_SCANCODE_UP])
+        {
+            if (selec == 0)
+                selec = 4;
+            else
+                selec -= 1;
+        }
 
-       else if (key[SDL_SCANCODE_DOWN])
-       {
-       if (selec == 4)
-       selec = PLAY;
-       else
-       selec += 1;
-       }
-       manage_update(selec, ga_st, enter_selec);
-       enter_selec = 0;
-    //}
-    }*/
+        else if (key[SDL_SCANCODE_DOWN])
+        {
+            if (selec == 4)
+                selec = 0;
+            else
+                selec += 1;
+        }
+        manage_update(selec, ga_st, enter_selec, &state);
+        enter_selec = 0;
+        //}
+    }
 
     SDL_DestroyTexture(ga_st->texture);
     SDL_FreeSurface(ga_st->surface);
