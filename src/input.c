b2e605e8351e16f6ef97d47bd652eca11b571291
Pia Consigny <pia.consigny@epita.fr>
2018-11-10 22:45:05 +0100

    Input player managment

--- src/input.c
+++ src/input.c
@@ -8,5 +8,17 @@ int input_update(struct game_state *ga_st)
         if (event.type == SDL_QUIT)
             return 1;
         if (event.type == SDL_KEYDOWN)
+            if (event.key.keysym.scancode == SDL_SCANCODE_SPACE)
+                player_jump(ga_st);
     }
+
+    const Uint8 *key = SDL_GetKeyboardState(NULL);
+
+    if (key[SDL_SCANCODE_D] || key[SDL_SCANCODE_RIGHT])
+        player_run(ga_st, 1);
+    else if (key[SDL_SCANCODE_A] || key[SDL_SCANCODE_LEFT])
+        player_run(ga_st, -1);
+    else
+        player_run(ga_st, 0);
+    return 0;
 }
