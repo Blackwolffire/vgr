d3776e1a8c5c3719ba956dddfca7a285fe668db4
Pia Consigny <pia.consigny@epita.fr>
2018-11-11 04:39:42 +0100

    Add other chunks

--- src/audio.c
+++ src/audio.c
@@ -3,6 +3,9 @@
 
 void play_chunk(struct game_state *ga_st, struct game_object *dec)
 {
-    if (dec->type == DEATH_BLOCK)
+    if (dec->type == DEATH_BLOCK || dec->type == ENNEMY ||
+        dec->type == PROJECTILE)
         Mix_PlayChannel(-1, ga_st->cri, 0);
+    if (dec->type == EXIT)
+        Mix_PlayChannel(-1, ga_st->win, 0);
 }
