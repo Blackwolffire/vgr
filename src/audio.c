24747c50f4d40001486c25e36b5905029f8c0be9
Pia Consigny <pia.consigny@epita.fr>
2018-11-11 04:48:27 +0100

    Modification enum name in audio.c

--- src/audio.c
+++ src/audio.c
@@ -3,7 +3,7 @@
 
 void play_chunk(struct game_state *ga_st, struct game_object *dec)
 {
-    if (dec->type == DEATH_BLOCK || dec->type == ENNEMY ||
+    if (dec->type == DEATH_BLOCK || dec->type == ENEMY ||
         dec->type == PROJECTILE)
         Mix_PlayChannel(-1, ga_st->cri, 0);
     if (dec->type == EXIT)
